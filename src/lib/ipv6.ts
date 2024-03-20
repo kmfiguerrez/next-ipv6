fa81c2e9025211a58a07f3cac930c9f72b5afd63
kmfiguerrez <kmfiguerrez@gmail.com>
2024-03-20 16:27:19 +0800

    Fix bug in isValidIPv6 method.

--- src/lib/ipv6.ts
+++ src/lib/ipv6.ts
@@ -1,4 +1,4 @@
-type TIPv6Result = {
+type IPv6ReturnData = {
   success: boolean
   message: string
 }
@@ -16,42 +16,38 @@ export class IPv6 {
     // Sanitize user input first.
     ipv6Address = ipv6Address.trim().toLowerCase()
 
+    // Regex pattern.
     // [0-9a-f]{1,4} means a segment of at least one and max at four of hex digits.
     // [0-9a-f]{1,4}: means a segment that ends with a semi-colon.
     // ([0-9a-f]{1,4}:){7} means a segment ends with a semi-colon, seven times.
     const completeIPv6AddPattern = /^([0-9a-f]{1,4}:){7}[0-9a-f]{1,4}$/
     let segments: RegExpMatchArray | null
     let regexPattern: RegExp
-    const ipv6Char = new Set<string>([
-      ":", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-      "a", "b", "c", "d", "e", "f"
-    ])
+    const ipv6CharPattern = /[^a-f0-9:]/
 
 
 
     if (ipv6Address === undefined || ipv6Address === null || ipv6Address === "") {
-      console.error("Invalid IPv6 format: IPv6 Address cannot be undefined, null nor an empty string.")
+      console.error("From isValidIPv6: Invalid IPv6 format - IPv6 Address cannot be undefined, null nor an empty string.")
       return false
     }
 
     // Check if user input uses valid ipv6 characters.
-    for (const char of ipv6Address) {
-      if (!ipv6Char.has(char)) {
-        console.error("Invalid IPv6 format: Not valid IPv6 character(s).")
-        return false
-      }
+    if (ipv6CharPattern.test(ipv6Address)) {
+      console.error("From isValidIPv6: Invalid IPv6 format - Not valid IPv6 character(s).")
+      return false
     }
 
     // IPv6 address must not start and end with a single colon.
     if ((ipv6Address[0] === ":" && ipv6Address.slice(0,2) !== "::") || (ipv6Address[ipv6Address.length - 1] === ":" && ipv6Address.slice(-2) !== "::")) { 
-      console.error("Invalid IPv6 format: Colon used at the beginning or end at the end.")
+      console.error("From isValidIPv6: Invalid IPv6 format - Colon used at the beginning or end at the end.")
       return false
     }
 
     // IPv6 address cannot have more than two contiguous colons.
     regexPattern = /:::/
     if (regexPattern.test(ipv6Address)) {
-      console.error("Invalid IPv6 format: Colon used more than twice contiguously.")
+      console.error("From isValidIPv6: Invalid IPv6 format - Colon used more than twice contiguously.")
       return false      
     }
 
@@ -60,7 +56,7 @@ export class IPv6 {
     const dcInstance = ipv6Address.match(regexPattern)
     if (dcInstance !== null) {
       if (dcInstance.length > 1) {
-        console.error("Invalid IPv6 format: Double-colon used more than once.")
+        console.error("From isValidIPv6: Invalid IPv6 format - Double-colon used more than once.")
         return false        
       }
     }
@@ -71,58 +67,62 @@ export class IPv6 {
     if (segments !== null) {
       for (const segment of segments) {
         if (segment.length > 4) {
-          console.error("Invalid IPv6 format: A segment can only have a max of four hex digits.")
+          console.error("From isValidIPv6: Invalid IPv6 format - A segment can only have a max of four hex digits.")
           return false  
         }
       }
     }
 
     // If double colon doesn't exist then a valid ipv6 address has an eight groups of segments and should only have a max of 7 colons.
-    // Otherwise used, then double-colon can only be used if there's two or more consecutive of segments of all zeros.
-    // So segments cannot be more than six.
+
     regexPattern = /::/
     if (!regexPattern.test(ipv6Address)) {
       if (!completeIPv6AddPattern.test(ipv6Address)) {
-        console.error("Invalid IPv6 format: IPv6 Address was not in complete format.")
+        console.error("From isValidIPv6: Invalid IPv6 format - IPv6 Address was not in complete format.")
         return false  
       }
     }
-    else {
+    
+    // Double-colon can only be used if there's two or more consecutive of segments of all zeros.
+    // So segments cannot be more than six.    
+    if (regexPattern.test(ipv6Address)) {
       // Get the number of segments.
       segments = ipv6Address.match(/[0-9a-f]{1,4}/g)
       if (segments !== null && segments.length > 6) {
-        console.error("Invalid IPv6 format: IPv6 Address with double-colon were not used in proper format.")
+        console.error("From isValidIPv6: Invalid IPv6 format - IPv6 Address with double-colon were not used in proper format.")
         return false  
       }
     }
 
     // Finally return true if all checkings passed.
-    console.log("Valid IPv6 Address.")
+    // console.log("Valid IPv6 Address.")
     return true
   }
   
 
   /**
-   * This method expands an abbreviated IPv6 address.
-   * By Adding leading zeros to segment.
-   * Turning :: into a segment of zeros.
+   * This method expands an abbreviated IPv6 address
+   * by adding leading zeros to segment and 
+   * turning :: into a segment of zeros.
    */
-  static expand(ipv6Address: string): TIPv6Result {
+  static expand(ipv6Address: string): IPv6ReturnData {
     // Sanitize user input first.
     ipv6Address = ipv6Address.trim().toLowerCase()
 
     // /[0-9a-f]{1,4}/ - Segment of hex digits
     let segments: RegExpMatchArray | null = ipv6Address.match(/[0-9a-f]{1,4}/g)
     let doubleColonPattern: RegExp = /^::$/
-    const expandedIPv6: TIPv6Result = {success: true, message: ""}    
+    const expandedIPv6: IPv6ReturnData = {success: true, message: ""}    
 
     try {
       // Check first if IPv6 Address is valid
-      if (!this.isValidIpv6(ipv6Address)) throw new Error("Invalid IPv6 Address")      
+      if (!this.isValidIpv6(ipv6Address)) throw new Error("From expand: Invalid IPv6 Address provided.")      
 
     } catch (error: unknown) {
-      expandedIPv6.success = false
-      expandedIPv6. message = "Invalid IPv6 Address"      
+      if (error instanceof Error) {
+        expandedIPv6.success = false
+        expandedIPv6. message = error.message
+      }
       return expandedIPv6
     }
 
@@ -172,11 +172,11 @@ export class IPv6 {
    * By removing leading zeros and Turning a sequence of segments of 
    * zeros.
    */
-  static abbreviate(ipv6Address: string): TIPv6Result {
+  static abbreviate(ipv6Address: string): IPv6ReturnData {
     // Sanitize user input first.
     ipv6Address = ipv6Address.trim().toLowerCase()
 
-    const abbreviatedIPv6: TIPv6Result = {success: true, message: ""}
+    const abbreviatedIPv6: IPv6ReturnData = {success: true, message: ""}
 
     let segments: Array<string>
 
