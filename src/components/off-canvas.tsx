576f03deebe56374bbf14687de51441116b6dc5a
kmfiguerrez <kmfiguerrez@gmail.com>
2024-03-20 18:50:20 +0800

    Add sheet comp from shadcn.

---
+++ src/components/off-canvas.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+type OffCanvasProps = {
+  children: React.ReactNode
+}
+
+const OffCanvas: React.FC<OffCanvasProps> = ({ children }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default OffCanvas
