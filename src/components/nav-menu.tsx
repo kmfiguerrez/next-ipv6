3150bcb200254f12ccb43a0165f7b002decc2475
kmfiguerrez <kmfiguerrez@gmail.com>
2024-04-04 18:34:12 +0800

    Add components in NavMenu file
    - Added components on NavMenu component for
      responsive and mobile navigation.

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
+
 import { Button } from "@/components/ui/button"
 
 import OffCanvas from "./features/off-canvas"
 
 import { ModeToggle } from "./theme-color/theme-toggler"
+
+import { cn } from "@/lib/utils"
+
+import { Menu, X } from "lucide-react"
+
+import gsap from "gsap"
+
 
 
 const NavMenu = () => {
@@ -10,28 +19,92 @@ const NavMenu = () => {
     <header className="mb-5 mt-1 max-sm:px-1 sm:container">
       <nav className='flex justify-between '>
         
-        <h1 className="flex items-center">IPv6 Subnetting</h1>
+        <h1 className="flex items-center font-semibold text-lg uppercase">IPv6 Subnetting</h1>
 
         <ModeToggle />
 
-        <div>
-          <OffCanvas>
-            <Button
-              variant={"link"}
-            >
-              Features
-            </Button>
-          </OffCanvas>
+        <NavLinks className="max-sm:hidden sm:flex sm:flex-row"/>
+        {/* Mobile links */}
+        <NavDropDownTrigger className="sm:hidden"/>
 
+      </nav>
+      {/* Nav dropdown content */}
+      <NavDropdownContent>
+        <NavLinks />
+      </NavDropdownContent>
+    </header>
+  )
+}
+
+
+const NavDropdownContent = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div id="navDropdownContent" className="h-0 overflow-hidden">
+      {children}
+    </div>
+  )
+}
+
+const NavLinks = ({className}: {className?: string}) => {
+  return (
+    <ul className={cn(``, className)}>
+      <li>
+        <OffCanvas>
           <Button
             variant={"link"}
           >
-            Info
+            Features
           </Button>
-        </div>
+        </OffCanvas>        
+      </li>
 
-      </nav>
-    </header>
+      <li>
+      <Button
+        variant={"link"}
+      >
+        Info
+      </Button>
+      </li>      
+    </ul>
+  )
+}
+
+const NavDropDownTrigger = ({className}: {className?: string}) => {
+  const [state, setState] = useState<"open" | "close">("close")
+
+  const handleClick = () => {
+    if (state === "close") {
+      setState("open")
+
+      // Get the nav dropdown content container.
+      const navDropdownContent = document.getElementById("navDropdownContent")
+      const navLinksElem = navDropdownContent?.children[0]
+      const navLinksHeight = navLinksElem?.getBoundingClientRect().height
+
+      // Animation.
+      gsap.to("#navDropdownContent", {height: navLinksHeight})
+      return
+    }
+    // Otherwise state is open.
+    setState("close")
+    // Animation.
+    gsap.to("#navDropdownContent", {height: 0})
+  }
+
+  return (
+    <Button
+      variant={"outline"}
+      size={"icon"}
+      onClick={handleClick}
+      className={cn(``, className)}
+    >
+      {state === "close" ? (
+          <Menu />
+        ): (
+          <X />
+        )
+      }
+    </Button>
   )
 }
 
